f00415f284a2c05888e35009f1d23a566c6de003
Sirio Sobrino Uranga <ssobrinou@MBP-Ssobrinou.local>
2019-05-07 13:42:26 +0200

    modified x and y scales

--- Classification/code/f_plot_roc.R
+++ Classification/code/f_plot_roc.R
@@ -1,5 +1,5 @@
 f_plot_roc<-function(real=real, predicted=test_tree, title=NULL){
   roc_curve<-roc(response=real,
                  predictor=predicted)
-  plot.roc(roc_curve, main=paste('ROC',title)); grid()
+  plot.roc(roc_curve, main=paste('ROC',title),xlim=c(1,0), ylim=c(0,1)); grid()
 }
